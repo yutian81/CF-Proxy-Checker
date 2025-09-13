e49e4082a3e06429cef1f09922e2836b3d898571
yutian81 <yutian88881@hotmail.com>
2025-09-13 20:26:03 +0800

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ define('ICO', 'https://cf-assets.www.cloudflare.com/dzlvafdwdttg/19kSkLSfWtDcspv
 define('TOKEN', '');
 define('URL302', '');
 define('URL', '');
-define('BEIAN', "<p>© 2025 <a href='./'>ProxyIP 检测合集</a> | <a href='https://www.serv00.com/'>由 Serv00 构建 | <a href='https://github.com/cmliu'>原作者 CMliu</a> | <a href='https://github.com/yutian81'>修改 Yutian81</a> | <a href='https://blog.811520.xyz'>QingYun Blog</a></p>");
+define('BEIAN', "<p>© 2025 <a href='./'>CF反代检测合集</a> | <a href='https://www.serv00.com/'>基于 Serv00 构建 | <a href='https://github.com/cmliu'>原作者 CMliu</a> | <a href='https://github.com/yutian81/CF-Proxy-Checker'>修改 Yutian81</a> | <a href='https://blog.811520.xyz'>QingYun Blog</a></p>");
 define('IMG', '');
 ?>
