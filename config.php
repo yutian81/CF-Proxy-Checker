813673a8210ebb9ed9199a68e50b3ba642e0a79b
yutian81 <yutian88881@hotmail.com>
2025-09-14 17:55:42 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,24 @@
 define('ICO', 'https://cf-assets.www.cloudflare.com/dzlvafdwdttg/19kSkLSfWtDcspvQI5pit4/c5630cf25d589a0de91978ca29486259/performance-acceleration-bolt.svg');
 define('TOKEN', '');
 define('URL302', '');
-define('BEIAN', "<p>© 2025 <a href='./'>ProxyIP 检测合集</a> | <a href='https://www.php.net'>基于 PHP 构建 | <a href='https://github.com/cmliu'>原作者 CMliu</a> | <a href='https://github.com/yutian81/CF-Proxy-Checker'>修改 Yutian81</a> | <a href='https://blog.811520.xyz'>QingYun Blog</a></p>");
 define('IMG', '');
+
+define('HEAD_FONTS', <<<'HTML'
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+HTML
+);
+
+define('BEIAN', <<<'HTML'
+<p>
+    © 2025 <a href="./">ProxyIP 检测合集</a> | 
+    <a href="https://www.php.net">基于 PHP 构建</a> | 
+    <a href="https://github.com/cmliu">原作者 CMliu</a> | 
+    <a href="https://github.com/yutian81/CF-Proxy-Checker">修改 Yutian81</a> | 
+    <a href="https://blog.811520.xyz">QingYun Blog</a>
+</p>
+HTML
+);
+
 ?>
